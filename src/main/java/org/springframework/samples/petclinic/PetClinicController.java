36fef71eab3f673917443138a70626190a50a7eb
Anas <anas@conzia.com>
2023-08-01 19:44:55 +0500

    sets implementation

--- src/main/java/org/springframework/samples/petclinic/PetClinicController.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicController.java
@@ -6,14 +6,17 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import com.example.request.SetElementRequest;
 
 import java.util.Map;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Set;
 
 @RestController
@@ -34,44 +37,83 @@ public class PetClinicController {
 
 	// Set Implementation
 
-	private Set<Object> uniqueSet = new HashSet<>();
+	private Map<String, Set<Object>> setMap = new HashMap<>();
 
-	@PostMapping("/addElement")
-	public ResponseEntity<String> addElement(@RequestBody Object element) {
-		if (uniqueSet.add(element)) {
-			return new ResponseEntity<>("Element added successfully.", HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>("Element already exists.", HttpStatus.OK);
+	@PostMapping("/{setName}/addElement")
+	public ResponseEntity<String> addElement(@PathVariable String setName, @RequestBody SetElementRequest request) {
+		Set<Object> set = setMap.computeIfAbsent(setName, k -> new HashSet<>());
+		Object elementToAdd = request.getElement();
+		if (set.add(elementToAdd)) {
+			return new ResponseEntity<>("Element added to the set '" + setName + "' successfully.", HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>("Element already exists in the set '" + setName + "'.", HttpStatus.OK);
 		}
 	}
 
-	@DeleteMapping("/removeElement")
-	public ResponseEntity<String> removeElement(@RequestBody Object element) {
-		if (uniqueSet.remove(element)) {
-			return new ResponseEntity<>("Element removed successfully.", HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>("Element not found.", HttpStatus.NOT_FOUND);
+	@DeleteMapping("/{setName}/removeElement")
+	public ResponseEntity<String> removeElement(@PathVariable String setName, @RequestBody SetElementRequest request) {
+		Set<Object> set = setMap.get(setName);
+		if (set == null) {
+			return new ResponseEntity<>("Set '" + setName + "' not found.", HttpStatus.NOT_FOUND);
+		}
+
+		Object elementToRemove = request.getElement();
+		if (set.remove(elementToRemove)) {
+			return new ResponseEntity<>("Element removed from the set '" + setName + "' successfully.", HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>("Element not found in the set '" + setName + "'.", HttpStatus.OK);
 		}
 	}
 
-	@GetMapping("/checkElement")
-	public ResponseEntity<String> checkElement(@RequestBody Object element) {
-		if (uniqueSet.contains(element)) {
-			return new ResponseEntity<>("Element exists in the Set.", HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>("Element does not exist in the Set.", HttpStatus.NOT_FOUND);
+	@GetMapping("/{setName}/checkElement")
+	public ResponseEntity<String> checkElement(@PathVariable String setName, @RequestBody SetElementRequest request) {
+		Set<Object> set = setMap.get(setName);
+		if (set == null) {
+			return new ResponseEntity<>("Set '" + setName + "' not found.", HttpStatus.NOT_FOUND);
+		}
+
+		Object elementToCheck = request.getElement();
+		if (set.contains(elementToCheck)) {
+			return new ResponseEntity<>("Element exists in the set '" + setName + "'.", HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>("Element does not exist in the set '" + setName + "'.", HttpStatus.NOT_FOUND);
 		}
 	}
 
-	@GetMapping("/getAllElements")
-	public ResponseEntity<Set<Object>> getAllElements() {
-		return new ResponseEntity<>(uniqueSet, HttpStatus.OK);
+	@GetMapping("/{setName}/getAllElements")
+	public ResponseEntity<Set<Object>> getAllElements(@PathVariable String setName) {
+		Set<Object> set = setMap.get(setName);
+		if (set != null) {
+			return new ResponseEntity<>(set, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
-	@DeleteMapping("/clearSet")
-	public ResponseEntity<String> clearSet() {
-		uniqueSet.clear();
-		return new ResponseEntity<>("Set cleared successfully.", HttpStatus.OK);
+	@DeleteMapping("/{setName}/clearSet")
+	public ResponseEntity<String> clearSet(@PathVariable String setName) {
+		Set<Object> set = setMap.get(setName);
+		if (set != null) {
+			set.clear();
+			return new ResponseEntity<>("Set '" + setName + "' cleared successfully.", HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>("Set '" + setName + "' not found.", HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@GetMapping("/getAllSets")
+	public ResponseEntity<Map<String, Set<Object>>> getAllSets() {
+		if (!setMap.isEmpty()) {
+			return new ResponseEntity<>(setMap, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
 	}
 
 }
---
+++ src/main/java/org/springframework/samples/petclinic/SetElementRequest.java
@@ -0,0 +1,15 @@
+package com.example.request;
+
+public class SetElementRequest<T> {
+
+	private T element;
+
+	public T getElement() {
+		return element;
+	}
+
+	public void setElement(T element) {
+		this.element = element;
+	}
+
+}
